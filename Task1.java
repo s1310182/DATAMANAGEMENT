7b3a5a910464efaa600d80a5f8d967398ac4b94c
s1310182 <s1310182@u-aizu.ac.jp>
2024-07-04 00:26:36 +0900

    HoT: initial revision

---
+++ Task1.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+class Task1 {
+    public static void main(String[] args) {
+
+        int tails = 0, heads = 0;
+        Random rand = new Random();
+
+        System.out.println("Tossing a coin...");
+
+        for (int i = 1; i <= 3; i++) {
+
+            System.out.print("Rountd " + i + " ");
+
+            int a = rand.nextInt(2);
+
+            if (a == 1) {
+                System.out.println("Heads");
+                heads++;
+            } else {
+                System.out.println("Tails");
+                tails++;
+            }
+        }
+        System.out.println("Heads: " + heads + ", Tails: " + tails);
+
+    }
+
+}
